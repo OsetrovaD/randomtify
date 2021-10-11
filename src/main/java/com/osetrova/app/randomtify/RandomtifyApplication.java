d6d2985a58256d85953a1704d8bf753c83490924
Darya Osetrova <liberty_20_07@mail.ru>
2021-10-11 22:02:07 +0300

    create base project

---
+++ src/main/java/com/osetrova/app/randomtify/RandomtifyApplication.java
@@ -0,0 +1,12 @@
+package com.osetrova.app.randomtify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomtifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomtifyApplication.class, args);
+    }
+}
