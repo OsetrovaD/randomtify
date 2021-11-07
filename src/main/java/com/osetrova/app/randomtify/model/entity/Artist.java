442e8a7b0302c7066a7ff4b6ce68594669162bd0
Darya Osetrova <liberty_20_07@mail.ru>
2021-11-07 20:25:32 +0300

    update artist entity

--- src/main/java/com/osetrova/app/randomtify/model/entity/Artist.java
+++ src/main/java/com/osetrova/app/randomtify/model/entity/Artist.java
@@ -1,27 +1,32 @@
 package com.osetrova.app.randomtify.model.entity;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import java.util.List;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Artist extends BaseEntity {
 
     @Column(unique = true, nullable = false)
     private String spotifyUrl;
 
-    @OneToMany
+    @Column(unique = true, nullable = false, updatable = false)
+    private String name;
+
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "artist_id")
+    @Fetch(FetchMode.SUBSELECT)
     private List<Song> songs;
 }
