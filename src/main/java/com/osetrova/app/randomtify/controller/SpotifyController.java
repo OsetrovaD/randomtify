65c3f418b423026f0c6b6c4f728add9e0d09097e
Darya Osetrova <liberty_20_07@mail.ru>
2022-01-10 20:45:55 +0300

    removed temporal test endpoint

--- src/main/java/com/osetrova/app/randomtify/controller/SpotifyController.java
+++ src/main/java/com/osetrova/app/randomtify/controller/SpotifyController.java
@@ -1,6 +1,5 @@
 package com.osetrova.app.randomtify.controller;
 
-import com.osetrova.app.randomtify.client.SpotifyClient;
 import com.osetrova.app.randomtify.model.dto.response.RandomArtistResponse;
 import com.osetrova.app.randomtify.model.search.Alphabet;
 import com.osetrova.app.randomtify.service.SearchService;
@@ -16,14 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/spotify/search")
 public class SpotifyController {
 
-    private final SpotifyClient spotifyClient;
     private final SearchService searchService;
-
-//    @PostMapping
-//    public SpotifySearchResponse search() {
-//        //test request
-//        return spotifyClient.findArtist("?query=tania+bowra&offset=0&limit=20&type=artist");
-//    }
 
     @GetMapping
     public ResponseEntity<RandomArtistResponse> getArtist(@RequestParam(required = false) Alphabet alphabet,
